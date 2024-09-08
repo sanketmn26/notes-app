383d9f49557bc66048e0b47805bb6e2fe6708a9d
sanketmn26 <sanketmn26@gmail.com>
2024-09-08 16:48:29 +0530

    Created mongoDb connection

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const connectDB = require("./server/config/db");
+const session = require('express-session');
 
 const app = express();
 const port = 5000 || process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// connect to database
+connectDB();
 
 // Static files
 app.use(express.static("public"));
@@ -17,17 +22,14 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-
 // Routes
 app.use("/", require("./server/routes/index"));
 app.use("/", require("./server/routes/dashboard"));
 
-
 // Handle 404
-app.get("*", function(req,res){
-  res.status(404).render("404")
-})
-
+app.get("*", function (req, res) {
+  res.status(404).render("404");
+});
 
 app.listen(port, () => {
   console.log(`App is listening on http://localhost:${port}`);
