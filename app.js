d2673cbee00cebf3bdc9b3af25e5283d0c83a3d3
sanketmn26 <sanketmn26@gmail.com>
2024-09-08 18:40:16 +0530

    passport-google-oauth implemented. Sign in with google implemented. Mongodb user schema created

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const connectDB = require("./server/config/db");
-const session = require('express-session');
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
 const app = express();
 const port = 5000 || process.env.PORT;
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URL,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,6 +39,7 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 // Routes
+app.use("/", require("./server/routes/auth"));
 app.use("/", require("./server/routes/index"));
 app.use("/", require("./server/routes/dashboard"));
 
